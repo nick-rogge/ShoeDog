856022767ae6e2e1b5e3078fedd4b70bae93c732
NickRogge <nickcrogge@gmail.com>
2025-03-21 21:25:18 -0500

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, {json} from 'express';
 import productsRoutes from './routes/products/index';
 
 const port = 3000;
 
 const app = express ();
+app.use(json());
 
 app.get('/', (req, res) => {
     res.send('Hello World123!');
