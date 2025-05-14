0e5fd6db88c615eb940b29be01356eae94966737
NickRogge <nickcrogge@gmail.com>
2025-05-14 14:20:57 -0400

    Product CRUD finished

--- api/src/db/productSchema.ts
+++ api/src/db/productSchema.ts
@@ -1,17 +1,26 @@
+import {
+    integer,
+    pgTable, 
+    varchar, 
+    text, 
+    doublePrecision, 
+    date
+} from 'drizzle-orm/pg-core';
+
 import { not } from 'drizzle-orm';
-import {integer, pgTable, varchar, text, doublePrecision, date} from 'drizzle-orm/pg-core';
+
 
 export const productsTable = pgTable('products', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    productType: varchar({length: 255}).notNull(),
-    brandName: varchar({length: 255}).notNull(),
+    productType: varchar({length: 255}),
+    brandName: varchar({length: 255}),
     name: varchar({length: 255}).notNull(),
-    description: text().notNull(),
+    description: text(),
     askPrice: doublePrecision().notNull(),
     authenticationDate: date(),
     aesEncryptionKey: varchar({length: 255}),
     styleCode: varchar({length: 255}),
-    size: varchar({length: 255}).notNull(),
+    size: varchar({length: 255}),
     colorway: varchar({length: 255}),
     image: varchar({length: 255}),
 })
